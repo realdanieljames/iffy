45d6390c09a6347c3ebdafd18296368fab0cc33a
DANIEL JAMES <daniel.james@codeimmersives.com>
2020-06-17 13:01:28 -0400

    both color-combinator.js and color-deconstructor.js are now interconnected through calling their functions in main.js

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 // Your code here!
 
+const colorCombinator = require('./color-combinator.js')
+const colorDeconstructor = require('./color-deconstructor.js')
+
+
+const one = getInput(0);
+const two = getInput(1);
+
+if (one === undefined){
+  console.log ("You need to enter a color")
+}
+
+if (one && two === undefined){
+  console.log (colorDeconstructor(one))
+}
+
+if (one && two){
+  console.log(colorCombinator(one, two))
+}
+
 
 
 // Our helper function here! Don't  touch!
